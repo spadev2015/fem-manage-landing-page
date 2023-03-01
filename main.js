1f01c313f39f80cfffdc58b5a37f4ebd3ce5b173
spadev2015 <43161944+spadev2015@users.noreply.github.com>
2023-03-01 09:59:14 -0800

    fixed slider and numbered list
    fixed slider and numbered list

--- main.js
+++ main.js
@@ -9,3 +9,32 @@ navToggle.addEventListener("click", () => {
     primaryNav.toggleAttribute("data-visible");
     primaryHeader.toggleAttribute("data-overlay");
 });
+
+const slider = new A11YSlider(document.querySelector('.slider'), {
+        adaptiveHeight: true,
+    //     dots: true,
+    //     centerMode: true,
+        arrows: false,
+        // slidesToShow: 3,
+        responsive: {
+            768: {
+                slidesToShow: 2,
+    //             dots: true,
+    //             arrows: false,
+    //             adaptiveHeight: true
+            },
+            960: { 
+    //             adaptiveHeight: true,
+                slidesToShow: 3,
+    //             disable: false, // slider disabled 960px to 1279px
+    //             dots: true,
+            },
+    //         1280: {
+    //             disable: false,
+    //             slidesToShow: 3,
+    //             dots: true, // dots enabled 1280px and up
+    //             arrows: false,
+    //             adaptiveHeight: true
+    //         }
+    }
+  });
